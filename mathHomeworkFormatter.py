f7b9bd6fad12211c43c7f329e698779aa2ceaba9
Siege <81945479+SiegeToaster@users.noreply.github.com>
2022-01-13 22:06:24 -0800

    HW titles formatted

--- mathHomeworkFormatter.py
+++ mathHomeworkFormatter.py
@@ -18,64 +18,37 @@ SCOPES = ['https://www.googleapis.com/auth/documents.readonly']
 DOCUMENT_ID = '1p21l34ih6o_aiOtmaZTnJ-fuXvU9W6nY5ZMDDXGSrYQ'
 TOKEN_PATH = "token.json"
 
-parser = argparse.ArgumentParser(description='Process Homework Numbers.')
-parser.add_argument('Numbers', metavar='N', type=int, nargs='+',
-                    help='The homework number(s) you want printed')
+parser = argparse.ArgumentParser(description='Process Homework Number.')
+parser.add_argument('Numbers', metavar='N', type=str, nargs='+',
+                    help='The homework number you want printed')
 
 args = parser.parse_args()
-# print(args.Numbers)
-
+REQUESTED_HW = ' '.join(args.Numbers)
 
 def main():
-	creds = auth()
+	document = None
 
 	try:
-		service = build('docs', 'v1', credentials=creds)
+		service = build('docs', 'v1', credentials=auth())
 
 		# Retrieve the documents contents from the Docs service.``
 		document = service.documents().get(documentId=DOCUMENT_ID).execute()
 		document = document.get('body').get('content')[4].get('table').get('tableRows')
 			# each index is a different assignment
-
-		for assignment in document:
-			assignment = assignment.get('tableCells')[0].get('content')
-			with open('content.json', 'w') as path:
-				path.write(json.dumps(assignment, indent = 4))
-
-			# print('\n')
-			hw_string = ''
-
-			for assignment_num in assignment:
-				assignment_num = assignment_num.get('paragraph').get('elements')[0].get('textRun').get('content')
-
-				# if assignment_num.lower().startswith('hw'):
-				# 	assignment_num = assignment_num[2:-1]
-				# if assignment_num.lower().startswith('test') or assignment_num.lower().startswith('chap'):
-				# 	assignment_num = assignment_num[4:-1]
-
-				if assignment_num.lower() == 'hw':
-					continue
-
-				if 'hw' in assignment_num.lower():
-					assignment_num = assignment_num[2:-1]
-
-				assignment_num = assignment_num.strip('\n')
-				assignment_num = assignment_num.strip()
-
-				hw_string += assignment_num + ' '
-				""" if len(re.findall("[0-9]+-[0-9]+", assignment_num)) > 0:
-					print(assignment_num)
-				elif len(re.findall("[0-9]+/[0-9]+", assignment_num)):
-					print(assignment_num)
-				elif len(re.findall("[0-9]+", assignment_num)) > 0:
-					print(assignment_num)
-				else:
-					print(assignment_num) """
-
-			hw_string = hw_string.strip()
-			print(hw_string)
+			
 	except HttpError as err:
 		print(err)
+
+	for assignment in document:
+		assignment = assignment.get('tableCells')[0].get('content')
+		hw_string = construct_hw_string(assignment)
+		valid_numbers = construct_valid_numbers(hw_string)
+		valid_numbers = filter_valid_numbers(valid_numbers, hw_string)
+
+		print(valid_numbers)
+		if REQUESTED_HW in valid_numbers:
+			# print(hw_string)
+			1 + 1
 
 def auth():
 	creds = None
@@ -97,6 +70,62 @@ def auth():
 			token.write(creds.to_json())
 
 	return creds
+
+def construct_hw_string(assignment):
+	hw_string = ''
+
+	for assignment_num in assignment:
+		assignment_num = assignment_num.get('paragraph').get('elements')[0].get('textRun').get('content')
+
+		if assignment_num.lower() == 'hw':
+			continue
+
+		if 'hw' in assignment_num.lower():
+			assignment_num = assignment_num[2:-1]
+
+		assignment_num = assignment_num.strip('\n')
+		assignment_num = assignment_num.strip()
+
+		hw_string += assignment_num + ' '
+
+	hw_string = hw_string.strip()
+	return hw_string
+
+def construct_valid_numbers(hw_string):
+	valid_numbers = []
+	if hw_string[0].isnumeric():
+		if '/' in hw_string:
+			valid_numbers = hw_string.split('/')
+		elif '-' in hw_string:
+			valid_numbers = hw_string.split('-')
+			for x in range(int(valid_numbers[0]) + 1, int(valid_numbers[1])):
+				valid_numbers.append(str(x))
+		else:
+			valid_numbers.append(hw_string)
+	else:
+		valid_numbers.append(hw_string)
+	
+	return valid_numbers
+
+def filter_valid_numbers(valid_numbers: list[str], hw_string: str):
+	for hw in valid_numbers:
+		if str(hw).isnumeric() or str(hw).lower().startswith('test') or str(hw).lower().startswith('chap'):
+			continue
+
+		if len(valid_numbers) <= 1 and not 'test' in str(hw).lower():
+			continue
+
+		# exceptions (tried to make robust but they are pretty unique): 
+		if len(valid_numbers) > 1:
+			valid_numbers = [hw_string]
+			continue
+
+		if 'test' in hw_string.lower():
+			index = valid_numbers[0].find(' ')
+			valid_numbers[0] = valid_numbers[0][0:index]
+			valid_numbers.append(hw_string[index:].strip())
+	
+	return valid_numbers
 
 if __name__ == '__main__':
 	main()
